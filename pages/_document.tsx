ffbc309f06922215e46b1fae2e012f5d17c044cb
yunpeng.song <yunpeng.song@ereachmobile.cn>
2024-05-09 14:54:38 +0800

    Update js

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,15 +1,16 @@
 import { Html, Head, Main, NextScript } from 'next/document';
+import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <script src='/js/jquery.min.js' />
+      <script defer src='/js/jquery.min.js' />
       <body>
         <Main />
         <NextScript />
       </body>
-      <script src='/js/global.js' />
+      <script defer src='/js/global.js' />
     </Html>
   );
 }
