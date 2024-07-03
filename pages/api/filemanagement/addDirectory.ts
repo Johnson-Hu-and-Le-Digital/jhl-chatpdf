1f1dc8b4b3427661d60b7e565f5b636b6d47544f
yunpeng.song <yunpeng.song@ereachmobile.cn>
2024-07-03 10:23:34 +0800

    Update add dir

--- pages/api/filemanagement/addDirectory.ts
+++ pages/api/filemanagement/addDirectory.ts
@@ -32,7 +32,7 @@ export default async function handler(
           spec: {
             serverless: {
               cloud: 'gcp',
-              region: PINECONE_ENVIRONMENT
+              region: 'us-east-4',
             }
           }
         });
